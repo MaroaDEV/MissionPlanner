0bc9339f783095b222be4810b143a58c7d124359
AlexisMGL <alexismg.lallement@gmail.com>
2024-05-01 19:27:22 +0200

    update top right png

--- ExtLibs/Utilities/Settings.cs
+++ ExtLibs/Utilities/Settings.cs
@@ -17,7 +17,7 @@ namespace MissionPlanner.Utilities
     {
         static Settings _instance;
 
-        public static string AppConfigName { get; set; } = "GCAM";
+        public static string AppConfigName { get; set; } = "Ground Control Software by AerialMetric";
 
         public static Settings Instance
         {
